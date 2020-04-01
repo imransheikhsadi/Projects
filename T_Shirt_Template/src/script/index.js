f765525e9ce31553918d6703888bdfb91b463836
Imran-S-heikh <imransheikhsadi@gmail.com>
2020-04-01 13:45:23 +0600

    Blog Section Done

--- T_Shirt_Template/src/script/index.js
+++ T_Shirt_Template/src/script/index.js
@@ -1,17 +1,22 @@
 import '../style/sass/main.scss';
 import Glide from '@glidejs/glide';
-import OnScreen from '../vendors/js/on_screen';
 
 
+const conf =  {
+  type: 'carousel',
+  startAt: 0,
+  perView: 3,
+  breakpoints: {
+    768: {
+      perView: 2
+    },
+    576: {
+      perView: 1
+    }
+  }
+}
 
 let glideOne = new Glide('.glide--One').mount();
-let onScreen = new OnScreen([
-    {
-        element: '.header',
-        callback: {
-            in: (element)=>{element.style.position = 'fixed'},
-            out: (element)=>{element.style.position = 'static'}
-        }
-    }
-])
+let glideTwo = new Glide('.glide--Two', conf ).mount();
+let glideThree = new Glide('.glide--Three', conf ).mount();
 
